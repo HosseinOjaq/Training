0b48081ea20d516b1818aa183a9ea8752b7bac54
Hossein Ojaq <hosseinojaq97@gmail.com>
2025-12-27 00:18:44 +0330

    add git ignore

--- obj/Debug/net9.0/Hossein.AssemblyInfo.cs
+++ obj/Debug/net9.0/Hossein.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hossein")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e808e20cd11fc28de0f07e61e15d889ced1c6d55")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hossein")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hossein")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
